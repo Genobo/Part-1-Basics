372172c4482590b4a1f7f6bea2ffca97d565497c
339516205 <339516205@2260I-L29-028>
2023-09-13 10:04:06 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Part_1_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Gabe";
+            string favMovie = "The Matrix";
+            Console.WriteLine($"Hello, {firstName}.");
+            Console.WriteLine($"Have you seen {favMovie}?");
+            Console.WriteLine("I have a fun trick! I'll see if the title contains 'The' in all caps. If it does, I'll print out 'true'.");
+            favMovie = favMovie.ToUpper();
+            Console.WriteLine($"{ favMovie.Contains("THE")}");
+            Console.WriteLine("I can replace letters as well. I'll show you!");
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+        }
+    }
+}
